a4fd545f5abf169d5a8b6f1ec50aa9c3526f02a1
Sebaister <Sebdrgn@live.cl>
2025-05-20 00:50:19 +0200

    refactor: simplify Vite config and update routing for GitHub Pages
    Removed the VitePWA plugin to simplify the configuration and updated the 404.html routing logic to use a more descriptive variable name. Also added a base tag in index.html to ensure proper routing on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'PokéDex 2025',
-        short_name: 'PokéDex',
-        description: 'La Enciclopedia Pokémon Definitiva',
-        theme_color: '#3D7DCA',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
-  ],
-  base: '/PokeWebV2/', // Esto es crucial para GitHub Pages
+  plugins: [react()],
+  base: '/PokeWebV2/',
 })
